4d846b1000fb28041f1ceb5a8505dc1a3fb3c6b6
Laurent Couvidou <lorancou@free.fr>
2020-01-12 16:48:54 +0100

    Forgot to split at new Data Types chapter

--- ch03-01-variables/src/main.rs
+++ ch03-01-variables/src/main.rs
@@ -14,19 +14,4 @@ fn main() {
     println!("The value of x is: {}", x);
     let x = x + 1;
     println!("The value of x is: {}", x);
-
-    // Tuple
-    let tup = (500, 6.4, 1);
-    let (_x, y, _z) = tup; // Makes 3 separate variables
-    println!("The value of y is: {}", y);
-
-    // Array    
-    let a = [1, 2, 3, 4, 5];
-    let index = 10;
-    let element = a[index]; // -> panic
-    println!("The value of element is: {}", element);
-
-    //const INDEX : usize = 10;
-    //let element = a[INDEX];
-    // => error on cargo build (*NOT* cargo check?!)
 }
