1a295c33463dd7cbc0c10b44a0e2b6d49c4f402b
Laurent Couvidou <lorancou@free.fr>
2020-01-12 17:00:38 +0100

    Functions

---
+++ ch03-03-functions/src/main.rs
@@ -0,0 +1,35 @@
+fn main() {
+    println!("Hello, world!");
+
+    another_function();
+
+    print_number(5);
+    print_numbers(5, 6);
+
+    let x = five();
+    println!("The value of x is: {}", x);
+
+    let y = plus_one(5);
+    println!("The value of y is: {}", y);
+}
+
+fn another_function() {
+    println!("Another function.");
+}
+
+fn print_number(x: i32) {
+    println!("The value of x is: {}", x);
+}
+
+fn print_numbers(x: i32, y: i32) {
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
