0a8129b4137747a3fa7c423df0eef96f137b64a9
Laurent Couvidou <lorancou@free.fr>
2020-01-14 21:54:18 +0100

    Command arguments + both ways

--- ch03-05-drill1-degrees/src/main.rs
+++ ch03-05-drill1-degrees/src/main.rs
@@ -1,20 +1,37 @@
-use std::io;
+use std::env;
 
-// https://www.rapidtables.com/convert/temperature/fahrenheit-to-celsius.html
 fn to_celsius(fahrenheit : f64) -> f64 {
     (fahrenheit - 32.0) * (5.0/9.0)
 }
 
+fn to_fahrenheit(celsius : f64) -> f64 {
+    (celsius * (9.0/5.0)) + 32.0
+}
+
 fn main() {
-    let mut fahrenheit = String::new();
+    let args: Vec<String> = env::args().collect();
 
-    println!("Enter a temperature in Fahrenheit:");
+    if args.len() != 3 {
+        println!("Wrong argument count.");
+        println!("Usage: degrees <n> <°C|°F>");
+        return;
+    }
 
-    io::stdin().read_line(&mut fahrenheit)
-        .expect("Failed to read line");
+    let n: f64 = match args[1].parse() {
+        Ok(n) => n,
+        Err(_) => {
+            println!("First argument must be a number.");
+	        println!("Usage: degrees <n> <°C|°F>");
+            return;
+        }
+    };
 
-    let fahrenheit: f64 = fahrenheit.trim().parse()
-    	.expect("Not a number");
-
-    println!("In Celsius: {}", to_celsius(fahrenheit));
+    match args[2].as_str() {
+	    "°C" => println!("{} °C = {} °F", n, to_fahrenheit(n)),
+	    "°F" => println!("{} °F = {} °C", n, to_celsius(n)),
+	    _ => {
+            println!("Second argument must be \"°C\" or \"°F\".");
+	        println!("Usage: degrees <n> <°C|°F>");
+	    },
+    }
 }
