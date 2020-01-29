96337fc8b7773a2b8b77ca5367e1cc7aa7c06cca
Laurent Couvidou <lorancou@free.fr>
2020-01-29 23:26:10 +0100

    Managing Growing Projects with Packages, Crates, and Modules

---
+++ ch07-00-restaurant/src/front_of_house.rs
@@ -0,0 +1,9 @@
+pub mod hosting;
+
+pub mod serving {
+    fn take_order() {}
+
+    pub fn serve_order() {}
+
+    fn take_payment() {}
+}
---
+++ ch07-00-restaurant/src/lib.rs
@@ -0,0 +1,67 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+mod front_of_house;
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::front_of_house::serving::serve_order();
+    }
+
+    fn cook_order() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
+// use crate::front_of_house::serving;
+// use front_of_house::serving; Same with a relative path
+pub use crate::front_of_house::serving; // Same but reexported
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+
+    // ...
+
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    // ...
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+
+    // ...
+
+    serving::serve_order();
+    serving::serve_order();
+    serving::serve_order();
+}
