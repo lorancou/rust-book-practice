fbe64fc1bcac65f094ab861207802b329ade16dc
Laurent Couvidou <lorancou@free.fr>
2020-02-09 20:47:08 +0100

    Unrecoverable Errors with panic!

---
+++ ch09-01-unrecoverable-errors-with-panic/src/main.rs
@@ -0,0 +1,15 @@
+// https://doc.rust-lang.org/book/ch09-01-unrecoverable-errors-with-panic.html
+
+fn main() {
+    //Unrecoverable Errors with panic!
+
+    //panic!("crash and burn");
+
+    // Using a panic! Backtrace
+
+    //let v = vec![1, 2, 3];
+    //v[99];
+
+    // Enable the backtrace with Powershell:
+    // > $Env:RUST_BACKTRACE=1
+}
