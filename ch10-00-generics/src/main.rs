9b3d802a14838a54779aa549664e254f1cc15b13
Laurent Couvidou <lorancou@free.fr>
2020-02-12 22:17:18 +0100

    Removing Duplication by Extracting a Function

---
+++ ch10-00-generics/src/main.rs
@@ -0,0 +1,52 @@
+// https://doc.rust-lang.org/book/ch10-00-generics.html
+
+fn main() {
+    // Removing Duplication by Extracting a Function
+
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let mut largest = number_list[0];
+
+    for number in number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    println!("The largest number is {}", largest);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let mut largest = number_list[0];
+
+    for number in number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    println!("The largest number is {}", largest);
+
+    // Extracting the code that finds the largest number into a function
+    fn find_largest(list: &[i32]) -> i32 {
+        let mut largest = list[0];
+
+        for &item in list {
+            if item > largest {
+                largest = item;
+            }
+        }
+
+        largest
+    }
+
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = find_largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let result = find_largest(&number_list);
+    println!("The largest number is {}", result);
+}
