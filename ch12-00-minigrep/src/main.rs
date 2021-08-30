6172ae23ac5ead19f011a8561654e637cf219acd
Laurent Couvidou <hello@lorancou.net>
2021-08-30 20:57:19 +0200

    An I/O Project: Building a Command Line Program

---
+++ ch12-00-minigrep/src/main.rs
@@ -0,0 +1,24 @@
+// https://doc.rust-lang.org/book/ch12-00-an-io-project.html
+
+// cargo run frog poem.txt
+// cargo run body poem.txt
+// cargo run monomorphization poem.txt
+
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
