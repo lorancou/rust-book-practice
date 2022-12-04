a315016fe8f8614d97636e4acb680c07e5a07e46
Laurent Couvidou <hello@lorancou.net>
2022-12-05 00:11:04 +0100

    Processing a Series of Items with Iterators - Updated

--- ch13-02-iterators/src/main.rs
+++ ch13-02-iterators/src/main.rs
@@ -1,6 +1,9 @@
+// Processing a Series of Items with Iterators
+// https://doc.rust-lang.org/book/ch13-02-iterators.html
+
+#![allow(dead_code)]
+
 fn main() {
-    // Processing a Series of Items with Iterators
-    
     let v1 = vec![1, 2, 3];
 
     let v1_iter = v1.iter();
@@ -92,51 +95,4 @@ fn filters_by_size() {
             },
         ]
     );
-}
-
-// Creating Our Own Iterators with the Iterator Trait
-
-struct Counter {
-    count: u32,
-}
-
-impl Counter {
-    fn new() -> Counter {
-        Counter { count: 0 }
-    }
-}
-
-impl Iterator for Counter {
-    type Item = u32;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.count < 5 {
-            self.count += 1;
-            Some(self.count)
-        } else {
-            None
-        }
-    }
-}
-
-#[test]
-fn calling_next_directly() {
-    let mut counter = Counter::new();
-
-    assert_eq!(counter.next(), Some(1));
-    assert_eq!(counter.next(), Some(2));
-    assert_eq!(counter.next(), Some(3));
-    assert_eq!(counter.next(), Some(4));
-    assert_eq!(counter.next(), Some(5));
-    assert_eq!(counter.next(), None);
-}
-
-#[test]
-fn using_other_iterator_trait_methods() {
-    let sum: u32 = Counter::new()
-        .zip(Counter::new().skip(1))
-        .map(|(a, b)| a * b)
-        .filter(|x| x % 3 == 0)
-        .sum();
-    assert_eq!(18, sum);
 }
