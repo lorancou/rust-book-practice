82347aecf4fd6a049bc71ee3415823960f8d08d5
Laurent Couvidou <hello@lorancou.net>
2022-12-05 21:52:28 +0100

    Improving Our I/O Project

---
+++ ch13-03-improving-our-io-project/src/main.rs
@@ -0,0 +1,23 @@
+// Improving Our I/O Project
+// https://doc.rust-lang.org/book/ch13-03-improving-our-io-project.html
+
+// cargo run frog poem.txt
+// cargo run body poem.txt
+// cargo run monomorphization poem.txt
+
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
